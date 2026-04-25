40802578fc3a0b48afd0a8ba7cc1db282bb23881
Hamza Mirza <hamza665@hotmail.de>
2026-04-25 10:59:03 +0200

    vbp palindrome completed

---
+++ src/woche_02/vbp_01/Main.java
@@ -0,0 +1,9 @@
+package woche_02.vbp_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Palindrome.checkPalindrome("a man, a plan, a canal: panama"));
+        System.out.println(Palindrome.checkPalindrome("a man, nama"));
+        System.out.println(Palindrome.checkPalindrome("anna"));
+    }
+}
