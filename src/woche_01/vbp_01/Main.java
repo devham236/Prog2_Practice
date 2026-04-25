06d0869320378cb818c1b459190ad74d9bc5b1b2
Hamza Mirza <hamza665@hotmail.de>
2026-04-25 09:22:06 +0200

    vbp task finished

--- src/woche_01/vbp_01/Main.java
+++ src/woche_01/vbp_01/Main.java
@@ -1,4 +1,23 @@
 package woche_01.vbp_01;
 
 public class Main {
+    public static void main(String[] args) {
+        Matrix m1 = new Matrix(2, 2);
+        int[][] m1_data = {{1, 3},
+                           {5, 7}};
+        m1.setData(m1_data);
+
+        Matrix m2 = new Matrix(2, 2);
+        int[][] m2_data = {{2, 4},
+                           {6, 8}};
+        m2.setData(m2_data);
+
+        Matrix result = m1.multiply(m2);
+
+        if (result != null) {
+            result.print();
+        } else {
+            System.out.println("Multiplikation nicht möglich.");
+        }
+    }
 }
--- src/woche_01/vbp_01/Matrix.java
+++ src/woche_01/vbp_01/Matrix.java
@@ -1,48 +1,45 @@
 package woche_01.vbp_01;
 
 public class Matrix {
-    public static void main(String[] args) {
-        int[][] matrix1 = {{1, 3},
-                          {5, 7}};
+    private int rows;
+    private int cols;
+    private int[][] data;
 
-        int[][] matrix2 = {{2, 4},
-                          {6, 8}};
-
-        int[][] test = multiply(matrix1, matrix2);
-
-        if (test != null){
-            for (int i = 0; i < test.length; i++) {
-                System.out.println();
-                for (int j = 0; j < test[i].length; j++) {
-                    System.out.printf("%d ", test[i][j]);
-                }
-            }
-        }
-        else {
-            System.out.println("Matrixmultiplikation nicht möglich");
-        }
+    public Matrix(int r, int c){
+        this.rows = r;
+        this.cols = c;
+        this.data = new int[r][c];
     }
 
-    public static int[][] multiply(int[][] m1, int[][] m2){
-        int rowsM1 = m1.length;
-        int rowsM2 = m2.length;
-        int colsM1 = m1[0].length;
-        int colsM2 = m2[0].length;
+    public void setData(int[][] values){
+        this.data = values;
+    }
 
-        if (rowsM1 != rowsM2 || colsM1 != colsM2){
+    public Matrix multiply(Matrix second){
+
+        if (this.rows != second.rows || this.cols != second.cols){
             return null;
         }
 
-        int[][] product = new int[m1.length][m1[0].length];
+        Matrix result = new Matrix(this.rows, second.cols);
 
-        for (int i = 0; i < m1.length; i++) {
-            for (int j = 0; j < m1[i].length; j++) {
-                for (int k = 0; k < m1[0].length; k++) {
-                    product[i][j] += (m1[i][k] * m2[k][j]);
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < second.cols; j++) {
+                for (int k = 0; k < this.cols; k++) {
+                    result.data[i][j] += (this.data[i][k] * second.data[k][j]);
                 }
             }
         }
 
-        return product;
+        return result;
+    }
+
+    public void print() {
+        for (int[] row : data) {
+            for (int val : row) {
+                System.out.printf("%d ", val);
+            }
+            System.out.println();
+        }
     }
 }
