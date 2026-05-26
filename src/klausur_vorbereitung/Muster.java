d7060ddd1e80807cb9575930975c90969dceac25
Hamza Mirza <hamza665@hotmail.de>
2026-05-26 11:16:15 +0200

    solution for christmas tree

--- src/klausur_vorbereitung/Muster.java
+++ src/klausur_vorbereitung/Muster.java
@@ -4,26 +4,33 @@ import java.util.Scanner;
 public class Muster {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int input;
 
-        System.out.print("Enter a number: ");
-        input = scanner.nextInt();
+        System.out.print("Enter number: ");
+        int input = scanner.nextInt();
 
-        for (int i = 1; i <= input; i++) {
-
-
-            for (int k = 0; k < i; k++) {
-                System.out.print("*");
-            }
-
-            for (int j = 0; j < input - i; j++) {
+        for (int i = 1; i <= input; i++){
+            for(int j = 0; j < input - i; j++){
                 System.out.print(" ");
             }
 
+            for (int k = 0; k < i; k++){
+                System.out.print("* ");
+            }
+
             System.out.println();
+
+            if(i == input){
+                for(int j = 0; j < i; j++){
+                    System.out.print(" ");
+
+                    if(j == input / 2){
+                        System.out.print("S");
+                    }
+                }
+                System.out.println();
+            }
         }
 
-        scanner.close();
 
     }
 }
