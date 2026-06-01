5564a739d767a60ad0a5646569b7db88c0467022
Hamza Mirza <hamza665@hotmail.de>
2026-06-01 14:33:29 +0200

    deleted box and muster folder and added tree class

--- src/klausur_vorbereitung/Tree.java
+++ src/klausur_vorbereitung/Tree.java
@@ -1,26 +1,24 @@
 package klausur_vorbereitung;
+
 import java.util.Scanner;
 
-public class Muster {
-    public static void main(String[] args) {
+public class Tree{
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number: ");
         int input = scanner.nextInt();
 
-        for(int i = 1; i <= input; i++){
+        for (int i = 1; i <= input ; i++) {
             for (int j = 0; j < input - i; j++) {
                 System.out.print(" ");
             }
 
-            for(int k = 0; k < 2 * i - 1; k++){
+            for (int k = 0; k < 2*i-1; k++) {
                 System.out.print("*");
             }
-
             System.out.println();
         }
 
-        for(int l = 0; l < input - 1; l++){
+        for (int i = 0; i < input - 1; i++) {
             System.out.print(" ");
         }
         System.out.println("*");
