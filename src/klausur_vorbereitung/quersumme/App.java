3a9ccbf305f8cc92a10684f242a64ee3c3f48e08
Hamza Mirza <hamza665@hotmail.de>
2026-07-19 14:53:03 +0200

    practice quersumme

--- src/klausur_vorbereitung/quersumme/App.java
+++ src/klausur_vorbereitung/quersumme/App.java
@@ -2,25 +2,19 @@ package klausur_vorbereitung.quersumme;
 
 import java.util.Scanner;
 
-public class App{
-    public static void main(String[] args){
-        System.out.println("Aufgabe 1 von Hamza Mirza, Start:");
+public class App {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nEingabe per Tastatur:");
+        System.out.println("Eingabe:");
         String input = scanner.nextLine();
 
         String[] strArr = input.split(" ");
-
         System.out.println("\nAusgabe:");
         for(int i = 0; i < strArr.length; i++){
             int num = Integer.parseInt(strArr[i]);
             int result = NumberAnalyser.analyse(num);
-
             System.out.println(String.format("%d hat die Quersumme %d", num, result));
         }
-
-        scanner.close();
-        System.out.println("\nAufgabe 1 Ende.");
     }
 }
--- src/klausur_vorbereitung/quersumme/NumberAnalyser.java
+++ src/klausur_vorbereitung/quersumme/NumberAnalyser.java
@@ -1,15 +1,14 @@
 package klausur_vorbereitung.quersumme;
 
-public class NumberAnalyser{
+public class NumberAnalyser {
     public static Integer analyse(Integer i){
         int sum = 0;
 
         while(i != 0){
             sum = i % 10 + sum;
-            i = i/10;
+            i = i /10;
         }
 
         return sum;
     }
 }
-
